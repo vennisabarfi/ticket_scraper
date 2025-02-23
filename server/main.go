8baa644f8b5c440846ceadc1752905bb1285c787
unknown <vennisabarfi@gmail.com>
2025-02-22 22:40:43 -0500

    set up gin server yayy! off to work on database schemas

--- server/main.go
+++ server/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 
+	r := gin.Default()
+
+	err := r.Run("0.0.0.0:3000")
+	if err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
+	fmt.Println("Server is up!")
 }
