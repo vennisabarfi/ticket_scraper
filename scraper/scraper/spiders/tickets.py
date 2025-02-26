5fc049db978a75aee0f546f1feb19d25ffe5b623
unknown <vennisabarfi@gmail.com>
2025-02-26 18:55:56 -0500

    working on getting ticket and show info. right now kendrick but need to extend and build a pipeline to automate this.

--- scraper/scraper/spiders/tickets.py
+++ scraper/scraper/spiders/tickets.py
@@ -3,25 +3,27 @@ from pathlib import Path
 import scrapy
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+class TicketSpider(scrapy.Spider):
+    name = "tickets"
     
     start_urls = [
-        "https://quotes.toscrape.com/page/1/",
-        "https://quotes.toscrape.com/page/2/",
+        # "https://quotes.toscrape.com/page/1/",
+        # "https://quotes.toscrape.com/page/2/",
+        "https://www.ticketmaster.com/search?q=kendrick+lamar",
     ]
         
     
-    # def parse(self, response):
-    #     page = response.url.split("/")[-2]
-    #     filename = f"quotes-{page}.html"
-    #     Path(filename).write_bytes(response.body)
-    #     self.log(f"Saved file {filename}")
-        
     def parse(self, response):
-        for quote in response.css("div.quote"):
-            yield {
-                "text": quote.css("span.text::text").get(),
-                "author": quote.css("small.author::text").get(),
-                "tags": quote.css("div.tags a.tag::text").getall(),
-            }
+        # page = response.url.split("/")[-1]
+        # filename = f"quotes-{page}.html"
+        filename = "kendrick.html"
+        Path(filename).write_bytes(response.body)
+        self.log(f"Saved file {filename}")
+        
+    # def parse(self, response):
+    #     for quote in response.css("div.quote"):
+    #         yield {
+    #             "text": quote.css("span.text::text").get(),
+    #             "author": quote.css("small.author::text").get(),
+    #             "tags": quote.css("div.tags a.tag::text").getall(),
+    #         }
